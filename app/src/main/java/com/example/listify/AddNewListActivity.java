c65da58ac3173c9b7db32b34977de2ab3a241c92
Milosz463 <miloszdawiec6@gmail.com>
2026-07-27 17:12:40 +0200

    dodanie wygladu czesci dodawania nowej listy

--- app/src/main/java/com/example/listify/AddNewListActivity.java
+++ app/src/main/java/com/example/listify/AddNewListActivity.java
@@ -1,6 +1,8 @@
 package com.example.listify;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddNewListActivity extends AppCompatActivity {
+ListView listViewNewLists;
+ArrayAdapter<String>adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,9 @@ public class AddNewListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listViewNewLists=findViewById(R.id.listView);
+        String[]elementy={""};
+        adapter=new ArrayAdapter<>(this,R.layout.list_view_style,elementy);
+        listViewNewLists.setAdapter(adapter);
     }
 }
