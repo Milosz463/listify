2d6797fc262bc74a073888718ff0bbecf7b5db26
Milosz463 <miloszdawiec6@gmail.com>
2026-08-05 12:26:40 +0200

    dodanie przycisku wroc na widok dodawania nowej listy

--- app/src/main/java/com/example/listify/AddListFragment.java
+++ app/src/main/java/com/example/listify/AddListFragment.java
@@ -3,12 +3,14 @@ package com.example.listify;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class AddListFragment extends Fragment {
@@ -22,9 +24,19 @@ public class AddListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_add_list,container,false);
         ListView listViewNewLists=view.findViewById(R.id.listView);
+        Button buttonGoBack=view.findViewById(R.id.buttonGoBack);
         String[]elementy={""};
         adapter=new ArrayAdapter<>(getContext(),R.layout.list_view_style,elementy);
         listViewNewLists.setAdapter(adapter);
+
+        buttonGoBack.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Navigation.findNavController(v).navigate(R.id.action_addListFragment_to_homeFragment);
+                    }
+                }
+        );
         return view;
     }
 }
